ac2127b5feb46d77515af2f4507232ec68ac9949
shaku2011 <n.h.27d2f5@gmail.com>
2024-03-28 15:37:50 +0900

    changed

--- bcilconf/settings.sh
+++ bcilconf/settings.sh
@@ -1,4 +1,6 @@
-export CARET7DIR=/mnt/pub/devel/workbench/release/1.5.0
-export HCPPIPEDIR=/mnt/pub/devel/NHPHCPPipeline
-export FREESURFER_HOME=/usr/local/freesurfer-v5.3.0-HCP
+export CARET7DIR=/usr/local/workbench/bin_linux64
+export HCPPIPEDIR=${SCRATCH}/HCP_project/HCPPipelines
+export FREESURFER_HOME=/usr/local/freesurfer
+export FSLDIR=/usr/local/fsl
+export DVARSDIR=${SCRATCH}/HCP_project/DVARS
 
