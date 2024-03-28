9e1553054545bff338dfb603ef378909bf37a714
Nobuaki Hondo <85020297+shaku2011@users.noreply.github.com>
2024-03-29 00:12:08 +0900

    Setting changed (#2)

--- bcilconf/settings.sh
+++ bcilconf/settings.sh
@@ -1,6 +1,7 @@
 export CARET7DIR=/usr/local/workbench/bin_linux64
 export HCPPIPEDIR=${SCRATCH}/HCP_project/HCPPipelines
 export FREESURFER_HOME=/usr/local/freesurfer
+source $FREESURFER_HOME/SetUpFreeSurfer.sh
 export FSLDIR=/usr/local/fsl
 export DVARSDIR=${SCRATCH}/HCP_project/DVARS
 
